cf9a0a374ed019a8383fcfa7a8a74b63d1e89e4d
Pyrrh <kurzen2@pdx.edu>
2018-11-26 19:38:22 -0800

    jshell intro

---
+++ Intro/HelloWorld.java
@@ -0,0 +1,10 @@
+System.out.println("Hello, World!")
+void main(String[] args) {
+System.out.println("Hello, world");
+}
+main(null)
+int NINE = 9;
+printf("Hello, World!")
+void printf(String format, Object...args) {System.out.printf(format,args);}
+printf("Hello, World!")
+class testClass {}
